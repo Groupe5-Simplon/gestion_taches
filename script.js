03ba38f7cebacd5e479a51ffdd1865f13a6a0892
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-05-06 01:37:02 +0100

    Added modifications

--- script.js
+++ script.js
@@ -11,36 +11,6 @@ close.addEventListener('click',function () {
     taskModel.style.display = 'none';
 })
 
-// const form = document.querySelector("form");
-// const tbody = document.querySelector("tbody");
-
-// function addIt(e) {
-//     e.preventDefault();
-//     alert('work');
-//     const titre = document.getElementById('title').value;
-//     const descript = document.getElementById('description').value;
-//     const date = document.getElementById('deadline').value;
-//     const etat = document.getElementById('etat').value;
-//     const priorite = document.getElementById('priority').value;
-//     // const row = body.insertRow();
-//     const edit = document.createElement('button');
-//     edit.innerHTML = '<ion-icon name="pencil-outline"></ion-icon>'
-//     edit.onclick = function(){
-//         editform(form);
-//     }
-
-//     const done = document.createElement('button');
-    
-//     done.innerHTML = '<ion-icon name="checkmark-done-circle-outline"></ion-icon>'
-//     done.onclick = function(){
-//         doneform(form);
-//     }
-// }
-
-// tbody.innerHTML += '`<tr><td>${titre}</td><td>${descript}</td><td>${date}</td><td>${etat}</td><td>${priorite}</td><td><button><ion-icon name="checkmark-done-circle-outline"></ion-icon></button></td><td><button><ion-icon name="create-outline"></ion-icon></button></td></tr>`';
-
-// form.addEventListener('submit', addIt);
-
 const formEl =document.querySelector("form");
 const tbodyEl =document.querySelector("tbody")
 const tableEl =document.querySelector("table");
@@ -59,8 +29,8 @@ function onAdd(e) {
             <td>${datel}</td>
             <td>${priorite}</td>
             <td>${Etat}</td>
-            <td class="g"><button class="btnM modi"  type="submit"><ion-icon name='pencil-outline'>"+"</ion-icon></button></td>
-            <td class="g"><button class="btnT termine" type="submit">Terminer</button></td>
+            <td class="g"><button type="submit" class="icon_click"><ion-icon name='pencil-outline' class="icon">"+"</ion-icon></button></td>
+            <td class="g"><button type="submit" class="icon_click2"><ion-icon name="checkmark-done-outline" class="icon"></ion-icon></button></td>
         </tr>
     `
     taskForm.reset();
